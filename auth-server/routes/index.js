afbe99c052323fb737749a5ee72d1de2c17fdb51
chahtk <chahtk@gmail.com>
2021-12-15 21:53:57 +0900

    feat: auto mail router

--- auth-server/routes/index.js
+++ auth-server/routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
 const router = express.Router();
+const authRouter = require('./auth');
 
 /* GET home page. */
-router.get('/', function (req, res) {
+router.get('/', (req, res) => {
   res.json({ id: 333, email: 'hello@saga' });
 });
+
+router.use('/auth', authRouter);
 
 module.exports = router;
