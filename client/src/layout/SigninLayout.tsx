317f3acf3a9381dc2bdba89aeb7bc21c663f6f12
chahtk <chahtk@gmail.com>
2021-12-16 09:41:19 +0900

    fix: naming

--- client/src/layout/SigninLayout.tsx
+++ client/src/layout/SigninLayout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import SigninContainer from '../container/SinginContainer';
+import SigninContainer from '../container/SigninContainer';
 import PutCenter from './PutCenter';
 
 const SigninLayout = () => {
