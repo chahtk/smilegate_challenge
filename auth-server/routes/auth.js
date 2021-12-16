4e184b472659e53f6d3b8e86b6ce16d01e37235f
chahtk <chahtk@gmail.com>
2021-12-16 13:24:10 +0900

    feat: seding mail logic

--- auth-server/routes/auth.js
+++ auth-server/routes/auth.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const codeGenerator = require('../utils/codeGenerator');
+const sendMail = require('../utils/sendMail');
 
 const router = express.Router();
 
-router.get('/email/:email', (req, res) => {
-  res.status(200).end();
+router.get('/email/:email', async (req, res) => {
+  const code = codeGenerator();
+  try {
+    await sendMail(req.params.email, code);
+    res.status(200).json({
+      status: 'sueccess',
+      code: 200,
+      message: 'refactoring...',
+    });
+  } catch (e) {
+    console.log(`Error: ${e}`);
+    res.status(400).end();
+  }
 });
 
 module.exports = router;
