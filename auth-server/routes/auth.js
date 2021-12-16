f644185f054e854a4b606249584d67fbd1d6c95b
chahtk <chahtk@gmail.com>
2021-12-16 15:05:13 +0900

    feat: auth code

--- auth-server/routes/auth.js
+++ auth-server/routes/auth.js
@@ -11,11 +11,26 @@ router.get('/email/:email', async (req, res) => {
   try {
     await sendMail(email, code);
     await redis.set(email, code);
-    res.status(200).json({
+    res.status(204).json({
       status: 'sueccess',
-      code: 200,
+      code: 204,
       message: 'refactoring...',
     });
+  } catch (e) {
+    console.log(`Error: ${e}`);
+    res.status(400).end();
+  }
+});
+
+router.get('/', async (req, res) => {
+  const { email, code } = req.query;
+  try {
+    const storedCode = await redis.get(email);
+    if (code === storedCode) {
+      res.status(204).end();
+    } else {
+      throw new Error('wrong code');
+    }
   } catch (e) {
     console.log(`Error: ${e}`);
     res.status(400).end();
