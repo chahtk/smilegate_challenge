1c559694d15dc2496baa7eb1882686e3026a7818
chahtk <chahtk@gmail.com>
2021-12-16 16:53:58 +0900

    remove useless

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from './modules';
 import PublicRoute from './routes/PublicRoute';
-import TestPage from './pages/TestPage';
 
 function App() {
   const userInfo = useSelector((state: RootState) => state.user);
--- client/src/pages/TestPage.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const TestPage = () => <div>login success</div>;
-
-export default TestPage;
