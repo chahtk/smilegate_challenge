9c4cd5d74d1e3dc2e2809b4c09efd3ba47f6397a
chahtk <chahtk@gmail.com>
2021-12-16 17:44:39 +0900

    fix: more time

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -5,7 +5,7 @@ interface LoadingProps {
   time?: number;
 }
 
-const Loading = ({ time = 3000 }: LoadingProps) => {
+const Loading = ({ time = 10000 }: LoadingProps) => {
   return <Loader type="Oval" color="#3d66ba" height={15} width={40} timeout={time} />;
 };
 
