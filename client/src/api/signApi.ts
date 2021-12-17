90a6193679885e3a338855a510795af9779f06a1
chahtk <chahtk@gmail.com>
2021-12-17 14:24:34 +0900

    feat: sign in api

--- client/src/api/signApi.ts
+++ client/src/api/signApi.ts
@@ -10,3 +10,14 @@ export const signUpApi = async (email: string, pass: string, userName: string): 
     return false;
   }
 };
+
+export const signInApi = async (email: string, pass: string): Promise<number | boolean> => {
+  const url = 'http://localhost:3001/sign/in';
+  const data = { email, pass };
+  try {
+    const response = await axios.post<AxiosResponse>(url, data);
+    return response.status;
+  } catch (e) {
+    return false;
+  }
+};
