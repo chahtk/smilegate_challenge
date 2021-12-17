acebf7b91fc714ab66274624b38356072de9242c
chahtk <chahtk@gmail.com>
2021-12-17 21:37:40 +0900

    fix: change page using redux state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from './modules';
 import PublicRoute from './routes/PublicRoute';
 import PrivateRoute from './routes/PrivateRoute';
 import getUserFromCookie from './utils/getUserFromCookie';
 
 function App() {
   const user = getUserFromCookie();
-  return <>{user ? <PrivateRoute /> : <PublicRoute />}</>;
+  const userInfo = useSelector((state: RootState) => state.user);
+
+  return <>{user && userInfo.login ? <PrivateRoute /> : <PublicRoute />}</>;
 }
 
 export default App;
