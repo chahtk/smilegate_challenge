785584a7920b3361bda68512d970fd8af653a35b
chahtk <chahtk@gmail.com>
2021-12-18 14:59:45 +0900

    feat: routing main page

--- client/src/routes/PrivateRoute.tsx
+++ client/src/routes/PrivateRoute.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Mainpage from '../pages/Mainpage';
 
 const PrivateRoute = () => {
   return (
     <Switch>
-      <Route exact path={'/'} render={() => <div>hello world</div>} />
+      <Route exact path={'/'} component={Mainpage} />
       <Redirect path="*" to="/"></Redirect>
     </Switch>
   );
